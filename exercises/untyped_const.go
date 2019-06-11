2c05bde603399655419218f106d2d47c2aee862c
deepak-sah <sah.sslpu@gmail.com>
2019-06-11 21:30:52 +0530

    Another commit

--- exercises/untyped_const.go
+++ exercises/untyped_const.go
@@ -5,13 +5,13 @@ import (
 )
 
 const (
-	a = 42
-	b = "hello"
-	c = 42.42
+	a  int = 42
+	b string = "hello"
+	c float64 = 42.42
 )
 
 func main() {
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%T\n", c)
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%s\n", b)
+	fmt.Printf("%f\n", c)
 }
