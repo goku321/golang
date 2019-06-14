183711206ae2dae466d8fd1b8c7f0661de5e60f3
deepak-sah <sah.sslpu@gmail.com>
2019-06-14 10:23:58 +0530

    Nailed it

--- exercises/using_struct.go
+++ exercises/using_struct.go
@@ -11,4 +11,12 @@ type person struct {
 
 func main() {
 	fmt.Println(person{"Deepak", 20})
+	fmt.Println(person{name: "Joe", age: 22})
+	fmt.Println(person{age: 30})
+	fmt.Println(&person{"Mike", 33})
+	s := person{"Brad", 34}
+	fmt.Println(s.name)
+	s.age = 42
+	fmt.Println(s.age)
+
 }
