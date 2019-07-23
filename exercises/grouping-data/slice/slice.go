8ecf660e18595864bbad3ccb574c0d925dd00177
Deepak <sah.sslpu@gmail.com>
2019-07-23 20:55:11 +0530

    Slicing a slice

--- exercises/grouping-data/slice/slice.go
+++ exercises/grouping-data/slice/slice.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	// Creating a slice using a COMPOSITE LITERAL
 	x := []int{1, 3, 4, 5}
 	
+	// Iterating over a slice using range clause
 	for i, v := range x {
 		fmt.Println(i, v)
 	}
+
+	// Slicing a slice using colon operator
+	slicedSlice := x[1:3]
+
+	fmt.Println(slicedSlice)
 }
