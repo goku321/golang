7d905a283066d052cb2cc36b12a7ec603f1e3cd1
Deepak <sah.sslpu@gmail.com>
2019-07-25 21:39:57 +0530

    Awesome job

--- exercises/grouping-data/slice/slice.go
+++ exercises/grouping-data/slice/slice.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	// Creating a slice using a COMPOSITE LITERAL
 	x := []int{1, 3, 4, 5}
-	
+
 	// Iterating over a slice using range clause
+	fmt.Printf("Iterating over slice: %v\n", x)
 	for i, v := range x {
 		fmt.Println(i, v)
 	}
@@ -14,5 +15,9 @@ func main() {
 	// Slicing a slice using colon operator
 	slicedSlice := x[1:3]
 
-	fmt.Println(slicedSlice)
+	fmt.Printf("Sliced slice: %v\n", slicedSlice)
+
+	// Appending to a slice using built-in `append`
+	x = append(x, 6, 7, 8, 9)
+	fmt.Printf("Slice after appending %v\n", x)
 }
