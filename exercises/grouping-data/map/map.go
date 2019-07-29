11c4451221ebd95a1a8f7432ffb16b713bccc8ac
Deepak <sah.sslpu@gmail.com>
2019-07-30 00:08:49 +0530

    Some progress made

---
+++ exercises/grouping-data/map/map.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Creating a string to int map
+	m := map[string]int{
+		"Deepak": 6,
+		"Sah": 3,
+	}
+
+	// Printing a map
+	fmt.Printf("Map m looks like: %v\n", m)
+}
