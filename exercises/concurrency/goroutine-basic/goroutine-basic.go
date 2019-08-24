e429affc2ca1d1d089bd1d3e4953a1d2745fa9ad
Deepak <sah.sslpu@gmail.com>
2019-08-24 19:24:09 +0530

    goroutines basics

---
+++ exercises/concurrency/goroutine-basic/goroutine-basic.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	// Allocate one logical processor
+	runtime.GOMAXPROCS(1)
+
+	// Use wg to wait for the program to finish
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Start Goroutines")
+
+	// Goroutine #1
+	go func() {
+		defer wg.Done()
+		for count := 0; count < 3; count++ {
+			for char := 'a'; char < 'a'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	// Goroutine #2
+	go func() {
+		defer wg.Done()
+		for count := 0; count < 3; count++ {
+			for char := 'A'; char < 'A'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	// wg.Wait()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Waiting to finish...")
+	}
+
+	wg.Wait()
+
+	fmt.Println("\n Terminating program")
+}
