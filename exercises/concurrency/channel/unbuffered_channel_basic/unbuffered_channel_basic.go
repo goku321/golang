6cc44d8cf32489d03c55631dbd6ed61d2a37b040
Deepak <sah.sslpu@gmail.com>
2019-10-08 18:39:44 +0530

    Unbuffered channel basic

--- exercises/concurrency/channel/unbuffered_channel_basic/unbuffered_channel_basic.go
+++ exercises/concurrency/channel/unbuffered_channel_basic/unbuffered_channel_basic.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	wg.Add(2)
 	unbuffered := make(chan int)
-	
+
 	go func() {
+		defer wg.Done()
 		unbuffered <- 1
 	}()
 
 	go func() {
+		defer wg.Done()
 		value := <-unbuffered
 		fmt.Println("Value received ", value)
 	}()
+
+	wg.Wait()
 }
