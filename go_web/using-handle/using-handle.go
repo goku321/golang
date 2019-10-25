10f83c559248a272f55f0b47b359a71b209d8a16
Deepak <sah.sslpu@gmail.com>
2019-10-25 23:40:42 +0530

    testing func Handle

---
+++ go_web/using-handle/using-handle.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type countHanlder struct {
+	mu sync.Mutex
+	n  int
+}
+
+func (h *countHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	h.n++
+	fmt.Fprintf(w, "Count is %d\n", h.n)
+}
+
+func main() {
+	http.Handle("/count", new(countHanlder))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
