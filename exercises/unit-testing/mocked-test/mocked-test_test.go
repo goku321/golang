89261e900fb2e3256baa93696f5f7d1dac9b27fa
Deepak <sah.sslpu@gmail.com>
2019-10-31 23:36:48 +0530

    Almost done writing test

--- exercises/unit-testing/mocked-test/mocked-test_test.go
+++ exercises/unit-testing/mocked-test/mocked-test_test.go
@@ -11,7 +11,20 @@ import (
 const checkMark = "u\2713"
 const ballotX = "u\2717"
 
-var feed = "adfad"
+var feed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss>
+<channel>
+    <title>Going Go Programming</title>
+    <description>Golang : https://github.com/goinggo</description>
+    <link>http://www.goinggo.net/</link>
+    <item>
+        <pubDate>Sun, 15 Mar 2015 15:04:00 +0000</pubDate>
+        <title>Object Oriented Programming Mechanics</title>
+        <description>Go is an object oriented language.</description>
+        <link>http://www.goinggo.net/2015/03/object-oriented</link>
+    </item>
+</channel>
+</rss>`
 
 func mockServer() *httptest.Server {
 	f := func(w http.ResponseWriter, r *http.Request) {
@@ -21,4 +34,33 @@ func mockServer() *httptest.Server {
 	}
 
 	return httptest.NewServer(http.HandlerFunc(f))
+}
+
+// TestDownload validates the HTTP Get function can download content.
+func TestDownload(t *testing.T) {
+	statusCode := http.StatusOK
+
+	server := mockServer()
+	defer server.Close()
+
+	t.Log("Given the need to test downloading content.")
+	{
+		t.Logf("\tWhen checking \"%s\" for status code \"%d\"", server.URL, statusCode)
+		{
+			resp, err := http.Get(server.URL)
+			if err != nil {
+				t.Fatal("\t\tShould be able to make the Get call.", ballotX, err)
+			}
+
+			t.Log("\t\tShould be able to make the Get call.", checkMark)
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode != statusCode {
+				t.Fatalf("\t\tShould receive a \"%d\" status. %v %v", statusCode, ballotX, resp.StatusCode)
+			}
+
+			t.Logf("\t\tShould receive a \"%d\" status. %v", statusCode, checkMark)
+		}
+	}
 }
