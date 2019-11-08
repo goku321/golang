53da364c6538453f4220fdde0702ffce315ea690
Deepak <sah.sslpu@gmail.com>
2019-11-08 22:33:14 +0530

    more on reflection

--- exercises/reflection/basic-reflection/basic-reflection.go
+++ exercises/reflection/basic-reflection/basic-reflection.go
@@ -9,4 +9,9 @@ func main() {
 	var x float64 = 3.4
 	fmt.Println("type:", reflect.TypeOf(x))
 	fmt.Println("value:", reflect.ValueOf(x).String())
+
+	v := reflect.ValueOf(x)
+	fmt.Println("type:", v.Type())
+	fmt.Println("kind is: ", v.Kind())
+	fmt.Println("kind is float64:", v.Kind() == reflect.Float64)
 }
