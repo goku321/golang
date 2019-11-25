4e5f8285a25c1ff90b234267890bc16d0391c7a1
Deepak <sah.sslpu@gmail.com>
2019-11-25 10:28:36 +0530

    test cases

--- exercises/unit-testing/endpoint-test/endpoint_test.go
+++ exercises/unit-testing/endpoint-test/endpoint_test.go
@@ -8,3 +8,24 @@ import (
 
 	"github.com/goinaction/code/chapter9/listing17/handlers"
 )
+
+const checkMark = "u\2713"
+const ballotX = "\u2717"
+
+func init() {
+	handlers.Routes()
+}
+
+func TestSendJSON(t *testing.T) {
+	t.Log("Given the need to test the SendJSON endpoint")
+	{
+		req, err := http.NewRequest("GET", "/sendjson", nil)
+		if err != nil {
+			t.Fatal("Should be able to create a request.",
+				ballotX, err)
+		}
+		t.Log("Should be able to create a request.", checkMark)
+
+		rw := httptest.NewRecorder()
+	}
+}
