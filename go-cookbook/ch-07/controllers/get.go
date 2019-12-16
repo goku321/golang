ec878939b8bdd12c16d37b61ab8faec34c8d189b
Deepak <sah.sslpu@gmail.com>
2019-12-16 23:48:18 +0530

    added get.go

---
+++ go-cookbook/ch-07/controllers/get.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// GetValue is a closure that wraps a HandlerFunc.
+func (c *Controller) GetValue(UseDefault bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		value := "default"
+		if !UseDefault {
+			value = c.storage.Get()
+		}
+		w.WriteHeader(http.StatusOK)
+		p := Payload{Value: value}
+		if payload, err := json.Marshal(p); err == nil {
+			w.Write(payload)
+		}
+	}
+}
