084439f751c8374ad4e064c97f0c2f18f1e17b9e
Deepak <sah.sslpu@gmail.com>
2019-12-16 23:08:48 +0530

    complete post.go

--- go-cookbook/ch-07/controllers/post.go
+++ go-cookbook/ch-07/controllers/post.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
+// SetValue modifies the underlying storage of the controller object.
 func (c *Controller) SetValue(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	return
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	value := r.FormValue("value")
+	c.storage.Put(value)
+	w.WriteHeader(http.StatusOK)
+	p := Payload{Value: value}
+	if payload, err := json.Marshal(p); err == nil {
+		w.Write(payload)
+	}
 }
