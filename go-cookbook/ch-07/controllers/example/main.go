0c525f7737a175ad0c62447757de8caf566c660b
Deepak <sah.sslpu@gmail.com>
2019-12-16 23:54:10 +0530

    added main.go

---
+++ go-cookbook/ch-07/controllers/example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang/go-cookbook/ch-07/controllers"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	storage := controllers.MemStorage{}
+	c := controllers.New(&storage)
+
+	http.HandleFunc("/get", c.GetValue(false))
+	http.HandleFunc("/get/default", c.GetValue(true))
+	http.HandleFunc("/set", c.SetValue)
+
+	fmt.Println("Listening on port :3333")
+	err := http.ListenAndServe(":3333", nil)
+	panic(err)
+}
