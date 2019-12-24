945d41cb9c1997dbead850b6a0c383d5ddb2bbc9
Deepak <sah.sslpu@gmail.com>
2019-12-24 23:14:03 +0530

    completed process.go

--- go-cookbook/ch-07/validation/process.go
+++ go-cookbook/ch-07/validation/process.go
@@ -1,11 +1,37 @@
 package validation
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Process ...
 func (c *Controller) Process(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	var p Payload
+
+	if err := decoder.Decode(&p); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := c.ValidatePayload(&p); err != nil {
+		switch err.(type) {
+		case Verror:
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 }
