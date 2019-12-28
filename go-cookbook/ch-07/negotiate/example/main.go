a4a70077ec111dad19076c7214ff8cc881846426
Deepak <sah.sslpu@gmail.com>
2019-12-28 22:56:26 +0530

    added main.go

---
+++ go-cookbook/ch-07/negotiate/example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"golang/go-cookbook/ch-07/negotiate"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", negotiate.Handler)
+	fmt.Println("listening on port :3333")
+	err := http.ListenAndServe(":3333", nil)
+	panic(err)
+}2020
