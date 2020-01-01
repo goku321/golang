3f1404dc25726adcee5bd0b6dcb7c1a1a4118bb2
Deepak <sah.sslpu@gmail.com>
2020-01-01 23:44:50 +0530

    finished middleware.go

--- go-cookbook/ch-07/middleware/middleware.go
+++ go-cookbook/ch-07/middleware/middleware.go
@@ -8,5 +8,8 @@ type Middleware func(http.HandlerFunc) http.HandlerFunc
 // ApplyMiddleware will apply all the middlewares
 func ApplyMiddleware(h http.HandlerFunc, middleware ...Middleware) http.HandlerFunc {
 	applied := h
+	for _, m := range middleware {
+		applied = m(applied)
+	}
 	return applied
 }
