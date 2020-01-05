1494acfba24d5d22663b03753b29e6086b29c410
Deepak <sah.sslpu@gmail.com>
2020-01-05 12:14:09 +0530

    added context.go

---
+++ go-cookbook/ch-07/middleware/context.go
@@ -0,0 +1,33 @@
+package middleware
+
+import "net/http"
+
+import "context"
+
+import "strconv"
+
+// ContextID is our type to retrieve our context
+type ContextID int
+
+// ID is the only ID we've defined
+const ID ContextID = 0
+
+// SetID ...
+func SetID(start int64) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), ID, strconv.FormatInt(start, 10))
+			start++
+			r = r.WithContext(ctx)
+			next(w, r)
+		}
+	}
+}
+
+// GetID ...
+func GetID(ctx context.Context) string {
+	if val, ok := ctx.Value(ID).(string); ok {
+		return val
+	}
+	return ""
+}
