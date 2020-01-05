52026e13096cdb4b2fd47ea8a8fb05b5faaecdc6
Deepak <sah.sslpu@gmail.com>
2020-01-05 21:27:55 +0530

    added main.go

---
+++ go-cookbook/ch-07/middleware/example/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"golang/go-cookbook/ch-07/middleware"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	h := middleware.ApplyMiddleware(
+		middleware.Handler,
+		middleware.Logger(log.New(os.Stdout, "", 0)),
+		middleware.SetID(100),
+	)
+
+	http.HandlerFunc("/", h)
+	fmt.Println("listening on port :3333")
+	err := http.ListenAndServe(":3333", nil)
+	panic(err)
+}
---
+++ go-cookbook/ch-07/middleware/handler.go
@@ -0,0 +1,9 @@
+package middleware
+
+import "net/http"
+
+// Handler is very basic
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("success"))
+}
