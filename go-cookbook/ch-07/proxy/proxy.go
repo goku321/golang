81bb072c8e142ff6c91c4c349ae8352f6bbd55de
Deepak <sah.sslpu@gmail.com>
2020-01-18 23:33:34 +0530

    adding ServeHTTP on proxy

--- go-cookbook/ch-07/proxy/proxy.go
+++ go-cookbook/ch-07/proxy/proxy.go
@@ -1,9 +1,20 @@
 package proxy
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 // Proxy holds configured client and BaseURL to proxy to
 type Proxy struct {
-	Client *http.Client
+	Client  *http.Client
 	BaseURL string
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := p.ProcessRequest(r); err != nil {
+		log.Printf("error occurred during processing request %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
