783ab2079610e3c2e72081523e9f210d1b0af453
Deepak <sah.sslpu@gmail.com>
2020-01-19 22:11:50 +0530

    added CopyResponse

--- go-cookbook/ch-07/proxy/process.go
+++ go-cookbook/ch-07/proxy/process.go
@@ -1,8 +1,10 @@
 package proxy
 
-import "net/http"
-
-import "net/url"
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+)
 
 // ProcessRequest modifies the request in accordance with the proxy settings
 func (p *Proxy) ProcessRequest(r *http.Request) error {
@@ -17,4 +19,20 @@ func (p *Proxy) ProcessRequest(r *http.Request) error {
 	r.RequestURI = ""
 
 	return nil
+}
+
+// CopyResponse takes the client response and writes everything to the
+// ResponseWriter in the original handler
+func CopyResponse(w http.ResponseWriter, resp *http.Response) {
+	var out bytes.Buffer
+	out.ReadFrom(resp.Body)
+
+	for k, v := range resp.Header {
+		for _, value := range v {
+			w.Header().Add(k, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	w.Write(out.Bytes())
 }
