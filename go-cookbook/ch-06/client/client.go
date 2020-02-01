60ceb2443267301c1fa072941d24fb22bdce388e
Deepak <sah.sslpu@gmail.com>
2020-02-01 22:43:55 +0530

    added client.go

---
+++ go-cookbook/ch-06/client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"crypto/tls"
+	"net/http"
+)
+
+// Setup configures our client and redefines
+// the global DefaultClient
+func Setup(isSecure, nop bool) *http.Client {
+	c := http.DefaultClient
+
+	if !isSecure {
+		c.Transport = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: false,
+			},
+		}
+	}
+
+	if nop {
+		c.Transport = &NopTransport{}
+	}
+
+	http.DefaultClient = c
+	return c
+}
+
+type NopTransport struct {}
+
+func (n *NopTransport) RoundTrip(*http.Request) (*http.Response, error) {
+	
+}
