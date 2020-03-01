44fbd2712bfe9236011856be538b8ae66cf8707a
Deepak <sah.sslpu@gmail.com>
2020-03-01 12:03:48 +0530

    basic middleware setup

--- fiber/main.go
+++ fiber/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+)
 
 func main() {
 	app := fiber.New()
+	app.Use(first, second)
 
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("Hello, World!")
 	})
 
 	app.Listen(3000)
+}
+
+var first = func(c *fiber.Ctx) {
+	fmt.Println("First Middleware")
+	c.Next()
+}
+
+var second = func(c *fiber.Ctx) {
+	fmt.Println("Second Middleware")
+	c.Next()
 }
