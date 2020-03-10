7e4c3a6a8b9662a93a2ef68c6b40ac0a972534c8
Deepak <sah.sslpu@gmail.com>
2020-03-10 21:19:26 +0530

    token exchange

--- go-cookbook/ch-06/oauthstore/config.go
+++ go-cookbook/ch-06/oauthstore/config.go
@@ -1,10 +1,26 @@
 package oauthstore
 
-import "golang.org/x/oauth2"
+import (
+	"context"
+
+	"golang.org/x/oauth2"
+)
 
 // Config wraps the default oauth2.Config
 // and adds storage
 type Config struct {
 	*oauth2.Config
 	Storage
+}
+
+// Exchange stores a token after retrieval
+func (c *Config) Exchange(ctx context.Context, code string) (*oauth2.Token, error) {
+	token, err := c.Config.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+	if err := c.Storage.SetToken(token); err != nil {
+		return nil, err
+	}
+	return token, nil
 }
