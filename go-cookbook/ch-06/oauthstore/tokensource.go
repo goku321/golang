492f8e37d80c9aaff260351ae586ad544a26828e
Deepak <sah.sslpu@gmail.com>
2020-03-12 22:44:32 +0530

    Token() on storageTokenSource

--- go-cookbook/ch-06/oauthstore/tokensource.go
+++ go-cookbook/ch-06/oauthstore/tokensource.go
@@ -11,6 +11,21 @@ type storageTokenSource struct {
 	oauth2.TokenSource
 }
 
+// Token satisfies the TokenSource interface
+func (s *storageTokenSource) Token() (*oauth2.Token, error) {
+	if token, err := s.Config.Storage.GetToken(); err == nil && token.Valid() {
+		return token, err
+	}
+	token, err := s.TokenSource.Token()
+	if err != nil {
+		return token, err
+	}
+	if err := s.Config.Storage.SetToken(token); err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
 // StorageTokenSource will be user by out configs
 // TokenSource function
 func StorageTokenSource(ctx context.Context, c *Config, t *oauth2.Token) oauth2.TokenSource {
