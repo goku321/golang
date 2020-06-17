147bb421d656fccb9f2e2d0155102392414b185a
Deepak <sah.sslpu@gmail.com>
2020-06-17 18:11:50 +0530

    remove unnecessary files

--- go_web/simple-handler/simple-handler.go
+++ go_web/simple-handler/simple-handler.go
@@ -11,13 +11,16 @@ import (
 type MyHandler struct{}
 
 func (h *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "I am being Served!")
+	fmt.Println("hey")
+	writer.WriteHeader(200)
+
+	writer.Write([]byte("hello world"))
 }
 
 func main() {
 	handler := MyHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8081",
 		Handler: &handler,
 	}
 	server.ListenAndServe()
