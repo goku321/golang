d90882723901ec61e69a056f39d148b0936c4944
goku321 <sah.sslpu@gmail.com>
2020-07-06 00:50:14 +0530

    falling back on default

--- exercises/struct/interface-embedding/interface-embedding.go
+++ exercises/struct/interface-embedding/interface-embedding.go
@@ -29,6 +29,14 @@ func (i z) put(s string) {
 	fmt.Println("from z: ", s)
 }
 
+type p struct {
+	x
+}
+
+func (i p) get() string {
+	return "from p"
+}
+
 func main() {
 	var a x   // x is an interface.
 	var c z   // z implements x.
@@ -43,4 +51,12 @@ func main() {
 	b.x.put("value")       // will call put method on z.
 	fmt.Println(b.get())   // since y doesn't explicitly defined get method, it fallbacks to z.get().
 	b.put("value")
+
+	t := p{c} // t is of type p and p embeds x. p also implements method get.
+	a = t
+
+	fmt.Println(a.get())
+	a.put("from t")
+
+	b.put("should fall back")
 }
