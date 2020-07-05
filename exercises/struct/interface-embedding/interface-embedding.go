4a32b891fe3708b6b720237ef28c19eb544421a5
goku321 <sah.sslpu@gmail.com>
2020-07-06 00:44:56 +0530

    more boggling

--- exercises/struct/interface-embedding/interface-embedding.go
+++ exercises/struct/interface-embedding/interface-embedding.go
@@ -7,38 +7,40 @@ type x interface {
 	put(string)
 }
 
-type z struct {}
-
-func (i z) put(s string) {
-	fmt.Println("putting: ", s)
-}
-
-func (i z) get() string {
-	return "nay"
-}
-
 type y struct {
 	x
 }
 
-func (i y) get() string {
-	return "yay"
+// func (i y) get() string {
+// 	return "yay"
+// }
+
+// func (i y) put(s string) {
+// 	fmt.Println("put put")
+// }
+
+type z struct{}
+
+func (i z) get() string {
+	return "from z"
 }
 
-func (i y) put(s string) {
-	fmt.Println("put put")
+func (i z) put(s string) {
+	fmt.Println("from z: ", s)
 }
 
 func main() {
-	var a x // is of type interface.
-	var c z
-	b := y{x: c} // is a struct.
-	a = b
+	var a x   // x is an interface.
+	var c z   // z implements x.
+	b := y{c} // injecting z for x.
 
-	fmt.Println(a.get())
-	a.put("value")
-	fmt.Println(b.get())
-	fmt.Println(b.x.get())
-	b.put("")
-	b.x.put("")
+	a = b // b is of type y and y embeds x, so it automatically implements x.
+
+	fmt.Println(a.get()) // will call get method on z.
+	a.put("value")       // will call put method on z.
+
+	fmt.Println(b.x.get()) // will call get method on z.
+	b.x.put("value")       // will call put method on z.
+	fmt.Println(b.get())   // since y doesn't explicitly defined get method, it fallbacks to z.get().
+	b.put("value")
 }
