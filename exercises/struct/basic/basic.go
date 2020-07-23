048a3a9b1e36ebee9124f15e7750866b6056ef36
goku321 <sah.sslpu@gmail.com>
2020-07-23 23:59:45 +0530

    some jedi stuff with structs

---
+++ exercises/struct/basic/basic.go
@@ -0,0 +1,21 @@
+package main
+
+import "log"
+
+type jedi struct {
+	name         string // name of the jedi.
+	midichlorins int    // midichlorins count
+	lightsaber   string // lightsaber color.
+}
+
+func main() {
+	var anakin jedi
+	// var definition assigns zero value?
+	log.Println(anakin, anakin.midichlorins)
+
+	luke := jedi{}
+	// struct comparison possible?
+	if anakin == luke {
+		log.Println("yes")
+	}
+}
