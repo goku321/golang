ed26132004d1c272b0f845716aaa726c6521648d
goku321 <sah.sslpu@gmail.com>
2020-07-24 00:05:39 +0530

    stoopid stuff with slices

---
+++ exercises/slice/basic/basic.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+func main() {
+	var x []int
+	// what is zero value of slice when defined using var?
+	log.Println(x)
+	// what about length and capacity?
+	log.Printf("len: %v, cap: %v", len(x), cap(x))
+	// how about getting a value using an index?
+	// log.Println(x[0]) // panics
+
+	// And when defined like this?
+	y := []int{}
+	log.Println(y)
+	log.Printf("len: %v, cap: %v", len(y), cap(y))
+	// let's try with y.
+	// log.Println(y[0]) // panics
+}
