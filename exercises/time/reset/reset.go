c1d12c57628e60f2c33b3e79dff454d0e00e472f
goku321 <sah.sslpu@gmail.com>
2020-07-24 23:54:33 +0530

    custom time resetter

--- exercises/time/reset/reset.go
+++ exercises/time/reset/reset.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+func resetTime(t time.Time) time.Time {
+	truncated := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	return truncated
+}
+
 func main() {
 	t, err := time.Parse(time.RFC3339, "2020-03-02T06:57:00.000Z")
 	if err != nil {
@@ -12,4 +17,12 @@ func main() {
 	}
 	// Truncate only works with UTC timezone.
 	log.Println(t.Truncate(time.Hour * 24))
+
+	// Let's try for a different timezone.
+	t = time.Now()
+	// Will it work?
+	log.Println(t.Truncate(time.Hour * 24))
+
+	// Let's try to with our custom function.
+	log.Println(resetTime(t))
 }
