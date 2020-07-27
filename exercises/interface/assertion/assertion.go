ee6e9c1a9793562e511d618e5fbccc5236011b6e
goku321 <sah.sslpu@gmail.com>
2020-07-27 23:45:11 +0530

    more interface assertion

--- exercises/interface/assertion/assertion.go
+++ exercises/interface/assertion/assertion.go
@@ -7,12 +7,28 @@ import (
 
 func main() {
 	var v interface{}
-	v = "4"
+	v = true
 
 	var val int
 	var ok bool
 	if val, ok = v.(int); !ok {
-		log.Fatalf("cannot assert value of type %s to an int value", reflect.TypeOf(v))
+		log.Printf("cannot assert value of type %s to an int value", reflect.TypeOf(v))
 	}
 	log.Println(val)
+
+	// Switch on interface value.
+	switch v {
+	case "4":
+		log.Println("switch works")
+	case true:
+		log.Println("works on booleans as well")
+	}
+
+	// let's try asserting an int value to float64.
+	v = 5
+	var fv float64
+	if fv, ok = v.(float64); !ok {
+		log.Printf("cannot assert value of type %s to float64 value", reflect.TypeOf(v))
+	}
+	log.Println(fv)
 }
