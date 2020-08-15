7ccc5476160b8d2ba46af30e17fcb6c0feaf5dd2
goku321 <sah.sslpu@gmail.com>
2020-08-16 01:18:07 +0530

    simple docker

--- go_web/simple-handler/simple-handler.go
+++ go_web/simple-handler/simple-handler.go
@@ -5,23 +5,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // MyHandler is an instance of Handler since it has a ServeHTTP method
 type MyHandler struct{}
 
 func (h *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("hey")
+	fmt.Printf("received request from %s\n", request.RemoteAddr)
+	host, err := os.Hostname()
+	if err != nil {
+		writer.WriteHeader(500)
+		writer.Write([]byte(err.Error()))
+		return
+	}
 	writer.WriteHeader(200)
 
-	writer.Write([]byte("hello world"))
+	writer.Write([]byte(fmt.Sprintf("hello world from %s", host)))
 }
 
 func main() {
 	handler := MyHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8081",
+		Addr:    "0.0.0.0:8081",
 		Handler: &handler,
 	}
+	fmt.Println("listening on :8081")
 	server.ListenAndServe()
 }
