5f0308c4b046c5c4783678fb0d7dd569b8c9101b
goku321 <sah.sslpu@gmail.com>
2020-08-27 00:15:24 +0530

    map: charcount - done

--- exercises/map/charcount/charcount.go
+++ exercises/map/charcount/charcount.go
@@ -31,4 +31,20 @@ func main() {
 		counts[r]++
 		utflen[n]++
 	}
+
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+
+	fmt.Printf("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
 }
