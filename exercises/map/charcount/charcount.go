6c4a006d688fe3d47cd408ddafd2395419426398
goku321 <sah.sslpu@gmail.com>
2020-08-26 23:57:56 +0530

    map: charcount - wip

---
+++ exercises/map/charcount/charcount.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
