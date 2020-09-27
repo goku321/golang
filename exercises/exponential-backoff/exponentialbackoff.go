035c4168d36dc377d595c8329eba460a3864452f
goku321 <sah.sslpu@gmail.com>
2020-09-27 20:44:32 +0530

    exp backoff

---
+++ exercises/exponential-backoff/exponentialbackoff.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func getData(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Get(url)
+		if err == nil {
+			return nil
+		}
+		log.Printf("server not responding (%s); retrying...", err)
+		time.Sleep(time.Second << uint(tries))
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
+}
+
+func main() {
+	getData("blah")
+}
